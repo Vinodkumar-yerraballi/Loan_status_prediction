6c7ff1d6d9b89ba72a7b44cc3e37fb9675ce1315
unknown <vinodkumar9676150@gmail.com>
2023-05-23 22:25:01 +0530

    update files

--- main.py
+++ main.py
@@ -11,9 +11,23 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/Prediction',methods=['GET','POST'])
 def prediction():
     if request.method == 'POST':
+        loan_id = request.POST.get('loan_id')
+        genders = request.POST.get('genders')
+        married = request.POST.get('married')
+        dependents = request.POST.get('dependents')
+        education = request.POST.get('education')
+        self_employed = request.POST.get('self_employed')
+        applicantincome = request.POST.get('applicantincome')
+        coapplicantincome = request.POST.get('coapplicantincome')
+        loanamount = request.POST.get('loanamount')
+        loan_amount_term = request.POST.get('loan_amount_term')
+        credit_history = request.POST.get('credit_history')
+        property_area = request.POST.get('property_area')
+
+        result=model.Prediction(loan_id,genders,married,dependents,education,self_employed,applicantincome,coapplicantincome,loanamount,loan_amount_term,credit_history,property_area)
         return render_template('prediction.html')
     
 
